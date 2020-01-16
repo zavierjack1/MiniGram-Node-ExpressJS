874dd4bc3f43886a9a9cd72f23a8e1a76e80eb95
Zavier Jack <zavierjack1@gmail.com>
2020-01-15 22:11:46 -0500

    hello world server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('Hello');
+const http = require('http'); //default node js pacakge
+
+const server = http.createServer((req, res) => {
+    res.end('this is my first response');
+});
+
+server.listen(process.env.PORT || 8080);
