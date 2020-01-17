cb231b9484de54f67a94ed2cbaa6a8257e9034db
Zavier Jack <zavierjack1@gmail.com>
2020-01-17 00:14:12 -0500

    added CORS

--- app.js
+++ app.js
@@ -2,15 +2,35 @@ const express = require('express');
 
 const app = express();
 
-//next just allows request to continue 
 app.use((req, res, next) => {
-    console.log("first middleware");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, PUT, OPTIONS'
+    )
     next();
-});
+})
 
-app.use((req, res, next) => {
-    //console.log("second middleware");
-    res.send('Hello from express!');
+app.use('/api/posts',(req, res, next) => {
+    const posts = [
+        {
+            id: 'some_random_id1',
+            title: 'first',
+            content: 'this is coming from the Express Server'
+        },
+        {
+            id: 'some_random_id2',
+            title: 'second',
+            content: 'this is the second post coming from the Express Server'
+        }
+    ];
+    res.status(200).json({
+        message: 'Post fetched successfully',
+        posts: posts
+    });
 });
 
 module.exports = app;
