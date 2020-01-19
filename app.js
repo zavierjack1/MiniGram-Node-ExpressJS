a7f888aa05627c6163d270976ec7431e8ed0cc67
Zavier Jack <zavierjack1@gmail.com>
2020-01-18 19:49:20 -0500

    added mongo htttp call to get

--- app.js
+++ app.js
@@ -43,22 +43,15 @@ app.post("/api/posts", (req, res, next) => {
 
 
 app.get('/api/posts',(req, res, next) => {
-    const posts = [
-        {
-            id: 'some_random_id1',
-            title: 'first',
-            content: 'this is coming from the Express Server'
-        },
-        {
-            id: 'some_random_id2',
-            title: 'second',
-            content: 'this is the second post coming from the Express Server'
-        }
-    ];
-    res.status(200).json({
-        message: 'Post fetched successfully',
-        posts: posts
-    });
+    Post.find()
+        .then(documents => {
+                console.log(documents);
+                res.status(200).json({
+                    message: 'Post fetched successfully',
+                    posts: documents
+                });
+            }
+        );
 });
 
 module.exports = app;
