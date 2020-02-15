f32b93915daeaf42238468ba6a5a2a7670dee2c7
Zavier Jack <zavierjack1@gmail.com>
2020-02-14 21:20:37 -0500

    added authentication

---
+++ middleware/check-auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try{
+        //expecting "Bearer this_is_my_token1231254245234234234"
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, "secret_this_should_be_longer")
+    }
+    catch{
+        res.status(401).json({
+            message: "Authentication failed"
+        })
+    }
+    
+
+
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const multer = require('multer')
 const router = express.Router();
+const multer = require('multer')
 const Post = require('../models/post');
+const checkAuth = require('../middleware/check-auth');
 
 const MIME_TYPE_MAP = {
     'image/png': 'png',
     'image/jpeg': 'jpg',
     'image/jpg': 'jpg'
 }
+
 const storage = multer.diskStorage(
     {
         destination: (req, file, callback) => {
@@ -74,61 +76,73 @@ router.get('/:id',(req, res, next) => {
         });
 });
 
-router.delete('/:id',(req, res, next) => {
-    Post.deleteOne({ _id: req.params.id }, function (err) {})
-        .then(result => {
-            res.status(200).json({
-                message: "Post deleted successfully"
-            });
-        }
-    );
+//protected 
+router.delete(
+    '/:id',
+    checkAuth, 
+    (req, res, next) => {
+        Post.deleteOne({ _id: req.params.id }, function (err) {})
+            .then(result => {
+                res.status(200).json({
+                    message: "Post deleted successfully"
+                });
+            }
+        );
 });
 
-router.post("", multer({storage: storage}).single("image"), (req, res, next) => {
-    const url = req.protocol + "://" + req.get("host");
-    const post = new Post(
-        {
-            title: req.body.title,
-            content: req.body.content,
-            imagePath: url + getImagePath(req.file.filename)
-        });
-    post.save().then(createdPost =>{
-        res.status(201).json({
-            message: "Post added successfully",
-            post: {
-                id: createdPost._id,
-                title: createdPost.title,
-                content: createdPost.content,
-                imagePath: createdPost.imagePath
-            }
-        });
-    });
-})
-
-router.put('/:id', multer({storage: storage}).single("image"), (req, res, next) =>{
-    //when we send put, if no file selected, the filePath will be a string in the body
-    let imagePath = req.body.imagePath;
-    //if new file uploaded, the filepath comes from the getImagePath
-    if (req.file){
+//protected
+router.post("", 
+    checkAuth, //dont execute function with () simply pass function
+    multer({storage: storage}).single("image"), 
+    (req, res, next) => {
         const url = req.protocol + "://" + req.get("host");
-        imagePath = url + getImagePath(req.file.filename)
-    }
-    const post = new Post({ 
-        _id: req.body.id, 
-        title: req.body.title,
-        content: req.body.content, 
-        imagePath: imagePath
-    });
-    Post.updateOne(
-            { _id: req.params.id }, 
-            post
-        )
-        .then(result => {
-            res.status(200).json({
-                message: "Post updated successfully",
-                post: post
+        const post = new Post(
+            {
+                title: req.body.title,
+                content: req.body.content,
+                imagePath: url + getImagePath(req.file.filename)
+            });
+        post.save().then(createdPost =>{
+            res.status(201).json({
+                message: "Post added successfully",
+                post: {
+                    id: createdPost._id,
+                    title: createdPost.title,
+                    content: createdPost.content,
+                    imagePath: createdPost.imagePath
+                }
             });
         });
+})
+
+//protected 
+router.put('/:id', 
+    checkAuth, 
+    multer({storage: storage}).single("image"), 
+    (req, res, next) =>{
+        //when we send put, if no file selected, the filePath will be a string in the body
+        let imagePath = req.body.imagePath;
+        //if new file uploaded, the filepath comes from the getImagePath
+        if (req.file){
+            const url = req.protocol + "://" + req.get("host");
+            imagePath = url + getImagePath(req.file.filename)
+        }
+        const post = new Post({ 
+            _id: req.body.id, 
+            title: req.body.title,
+            content: req.body.content, 
+            imagePath: imagePath
+        });
+        Post.updateOne(
+                { _id: req.params.id }, 
+                post
+            )
+            .then(result => {
+                res.status(200).json({
+                    message: "Post updated successfully",
+                    post: post
+                });
+            });
 })
 
 module.exports = router;
