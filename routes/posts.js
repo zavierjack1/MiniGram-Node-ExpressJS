808072af28445e3c618d838a67f74b9ad3cb2b81
Zavier Jack <zavierjack1@gmail.com>
2020-02-29 11:38:18 -0500

    seperated multer logic from route

--- routes/posts.js
+++ routes/posts.js
@@ -1,32 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
+const extractFile = require('../middleware/extract-file');
 const postsController = require('../controller/posts');
-const multer = require('multer')
-
-const MIME_TYPE_MAP = {
-    'image/png': 'png',
-    'image/jpeg': 'jpg',
-    'image/jpg': 'jpg'
-};
-
-const storage = multer.diskStorage(
-    {
-        destination: (req, file, callback) => {
-            const isValid = MIME_TYPE_MAP[file.mimetype];
-            let error = new Error('invalid mime type');
-            if (isValid){
-                error = null;
-            }
-            callback(error, 'images') //path relative to server.js
-        }, 
-        filename: (req, file, callback) => {
-            const name = file.originalname.toLowerCase().split(' ').join('-');
-            const ext = MIME_TYPE_MAP[file.mimetype];
-            callback(null, name + '-' + Date.now() + '.' + ext);
-        }
-    }
-);
 
 router.get('', postsController.getPosts);
 router.get('/:id', postsController.getPostById);
@@ -36,18 +12,9 @@ router.get('/:id', postsController.getPostById);
 router.delete('/:id', checkAuth, postsController.deletePostById);
 
 //protected - logged in users only
-router.post("", 
-    checkAuth, 
-    multer({storage: storage}).single("image"), 
-    postsController.createPost
-);
+router.post("", checkAuth, extractFile, postsController.createPost);
 
 //protected - logged in users only 
-//only user who created should be able to update
-router.put('/:id', 
-    checkAuth, 
-    multer({storage: storage}).single("image"), 
-    postsController.updatePostById
-);
+router.put('/:id', checkAuth, extractFile, postsController.updatePostById);
 
 module.exports = router;
